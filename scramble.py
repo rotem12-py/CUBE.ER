82a5af0196f39306e328ea707b4889c1eb09949e
Rotem Ben Israel <rotembeni12@gmail.com>
2024-12-04 18:38:26 +0200

    Add 2x2 Cube And Optimize Scramble Func

--- scramble.py
+++ scramble.py
@@ -1,26 +1,34 @@
 import random
 
 # possible moves for every cube type
+x2_chars = [" U", " U'", " U2", " R", " R2", " R'", " F", " F'", " F2"]
 x3_chars = [' U', " U'", ' U2' , ' D', " D'", ' D2', ' L', " L'", ' L2' , ' R', " R", ' R2', ' F', " F'", ' F2', ' B', " B'", ' B2']
 
-# return the scramble according to the type passed
-def scramble(scr_type):
-    if scr_type == "3x3":
-        return x3()
 
-def x3():
+def scramble(scr_type):
     result = []
     result_txt = ""
-    for i in range(20):
-        chosen_move = random.choice(x3_chars)
-        if len(result) > 0:
-            if result[-1][1] != chosen_move[1]:
-                result.append(chosen_move)
-            else:
-                chosen_move = random.choice(x3_chars)
-                result.append(chosen_move)
-        else:
-            result.append(chosen_move)
+    scramble_chars = []
+    scr_length = 0
+
+    # set the scramble possible characters and length depending on scr_type
+    if scr_type == "3x3":
+        scramble_chars = x3_chars
+        scr_length = 20
+    elif scr_type == "2x2":
+        scramble_chars = x2_chars
+        scr_length = 9
+
+    # add a random char to the scramble from the possible characters
+    for i in range(scr_length):
+        chosen_move = random.choice(scramble_chars)
+
+        # if the new movie has the same letter as the same one, replace it
+        while len(result) > 0 and chosen_move[1] == result[-1][1]:
+            chosen_move = random.choice(scramble_chars)
+
+        result.append(chosen_move)
+
     for i in result:
         result_txt += i
     return result_txt
